88059b7e24ccd35f4c61159af2c5bb987f885315
sarahb <sarah.buchhorn@gmail.com>
2019-02-19 23:28:52 -0600

    Initial commit

---
+++ data/sites.js
@@ -0,0 +1,185 @@
+var sites = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "site_id": 1,
+        "name": "Bahai Temple",
+        "waterbody": "North Branch Chicago River"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -87.686166,
+          42.073716999999995
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "site_id": 2,
+        "name": "Dammrich Rowing Center",
+        "waterbody": "North Branch Chicago River"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -87.709899,
+          42.027903
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "site_id": 3,
+        "name": "Foster Ave",
+        "waterbody": "North Branch Chicago River"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -87.704607,
+          41.975704
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "site_id": 4,
+        "name": "Irving Park Rd",
+        "waterbody": "North Branch Chicago River"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -87.6944,
+          41.95406
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "site_id": 5,
+        "name": "Belmont Ave",
+        "waterbody": "North Branch Chicago River"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -87.69242,
+          41.939409999999995
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "site_id": 6,
+        "name": "Diversey Ave",
+        "waterbody": "North Branch Chicago River"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -87.68273,
+          41.93215
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "site_id": 7,
+        "name": "North Ave",
+        "waterbody": "North Branch Chicago River"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -87.657004,
+          41.910933
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "site_id": 8,
+        "name": "Merchandise Mart",
+        "waterbody": "North Branch Chicago River"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -87.63978,
+          41.88906
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "site_id": 9,
+        "name": "Ping Tom Park",
+        "waterbody": "South Branch Chicago River"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -87.63511,
+          41.857890000000005
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "site_id": 10,
+        "name": "Eleanor Boat House",
+        "waterbody": "South Branch Chicago River"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -87.663793,
+          41.843472
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "site_id": 11,
+        "name": "Bridgeport Art Center",
+        "waterbody": "South Fork South Branch Chicago River"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -87.657275,
+          41.830498999999996
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "site_id": 12,
+        "name": "Kedzie near 31st",
+        "waterbody": "Sanitary and Ship Canal"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -87.70476,
+          41.831608
+        ]
+      }
+    }
+  ]
+}
---
+++ watermap.js
@@ -0,0 +1,54 @@
+var mymap = L.map('watermap').setView([41.945266, -87.669750], 10)
+
+var mytoken = 'pk.eyJ1Ijoic2NiMDIwMTAiLCJhIjoiY2pzM2Y2eHdjMmVuaTQ1bzN6OGE3MnJrYiJ9.5QDjNpLmtS-Y9N3nP2rLdQ'
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}',{
+    maxZoom: 18,
+    id: 'mapbox.light',
+    accessToken: mytoken, 
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+}).addTo(mymap);
+
+var geojsonMarkerOptions = {
+    radius: 8,
+    fillColor: "#ff7800:",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+};
+
+samplingsites = L.geoJSON(sites, {
+    onEachFeature: function(feature, layer) {
+        layer.bindTooltip(feature.properties.name);
+        layer.on('mouseover',function() {layer.openTooltip(); });
+        layer.on('mouseout',function() {layer.closeTooltip(); });
+},
+    pointToLayer: function(feature,latlng) {
+        return L.circleMarker(latlng, geojsonMarkerOptions);
+    }
+}).addTo(mymap);
+
+function featureJoinByProperty(fProps, dTable, joinKey) {
+    var keyVal = fProps[joinKey];
+    var match = {};
+    for (var i = 0; i < dTable.length; i++) {
+        if (dTable[i][joinKey] == keyVal) {
+            match = dTable[i];
+            for (key in match) {
+                if (!(key in fProps)) {
+                    fProps[key] = match[key];
+                }
+            }
+        }
+    }
+}
+
+samplingsites.eachLayer(function (layer) {
+    featureJoinByProperty(layer.feature.properties, data, 'site_id');
+    console.log(layer.feature.properties.ph);
+});
+
+// how do i perform operations on a selection/use the joined data to populate the tooltip?
