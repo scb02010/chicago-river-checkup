10664825b4b411d64315041bd138daef54a9985d
sarahb <sarah.buchhorn@gmail.com>
2019-04-01 22:18:19 -0500

    remove extra imports

--- app/controllers/readingscontroller.py
+++ app/controllers/readingscontroller.py
@@ -1,14 +1,6 @@
 from flask_restful import Resource
 from flask import send_from_directory, request, json, render_template, url_for
-
-import sqlite3
-
-import click
-from flask import app, g
-from flask.cli import with_appcontext
-
 from app.models.readings import Readings
-from flask import jsonify
 
 
 class ReadingsController(Resource):
