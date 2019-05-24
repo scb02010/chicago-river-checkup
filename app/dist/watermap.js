36d7f100ac5705715bde75dfe686caeb343b6905
sarahb <sarah.buchhorn@gmail.com>
2019-05-24 09:21:36 -0500

    legend and graph style

--- app/dist/watermap.js
+++ app/dist/watermap.js
@@ -41,10 +41,12 @@ function setupmap() {
                 return L.marker(latlng,geojsonMarkerOptions);
             }
         }).addTo(mymap);
+        legend.addTo(mymap);
     })
 };
 
 function cleanmap() {
+    legend.remove(mymap);
     mymap.eachLayer(function (layer) {
         if (layer.mytag == "geoJSON") {
             mymap.removeLayer(layer);
@@ -59,8 +61,69 @@ var siteids = [];
 var activeparam = 'ph';
 var dataset = [];
 
-//testing
-//get icons based on difficulty rating
+
+var paramcutoffs = {
+    ph: {grades: [0,6.5,8,9], 
+        colors: [
+        "rgba(255,255,0,0.6)",
+        "rgba(16, 219, 107, 0.6)",
+        "rgba(255,255,0,0.6)",
+        "rgba(255, 9, 9, 0.6)" 
+    ]}, 
+    do: {grades: [0, 3, 6],
+        colors: [
+            "rgba(255, 9, 9, 0.6)",
+            "rgba(255,255,0,0.6)",
+            "rgba(16, 219, 107, 0.6)"
+        ]}, 
+    phosphate: {grades: [0, 0.3, 0.6], 
+        colors: [
+            "rgba(16, 219, 107, 0.6)",
+            "rgba(255,255,0,0.6)",
+            "rgba(255, 9, 9, 0.6)"
+        ]},
+    conductivity: {grades: [0,150,500,1900],
+        colors: [
+            "rgba(255,255,0,0.6)",
+            "rgba(16, 219, 107, 0.6)",
+            "rgba(255,255,0,0.6)",
+            "rgba(255, 9, 9, 0.6)"
+    ]},
+    tempcelsius: {grades: [0,30], 
+        colors: [
+            "rgba(16, 219, 107, 0.6)",
+            "rgba(255, 9, 9, 0.6)"
+        ]}
+
+    }
+
+
+var legend = L.control({position: 'topright'});
+
+legend.onAdd = function (mymap) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = paramcutoffs[activeparam].grades,
+        colors = paramcutoffs[activeparam].colors,
+        labels = [],
+        from, to;
+
+    for (var i = 0; i < grades.length; i++) {
+        from = grades[i];
+        to = grades[i + 1];
+
+        labels.push(
+            '<i style="background:' + colors[i] + '"></i> ' +
+            from + (to ? '&ndash;' + to : '+'));
+    }
+
+    div.innerHTML = activeparam + '<br>' + labels.join('<br>');
+    return div;
+};
+
+legend.addTo(mymap);
+
+// color by most recent reading
 function getIcon(rating) {
     if (activeparam == 'ph') {
         return rating < 6.5 ? redIcon :
@@ -81,7 +144,6 @@ function getIcon(rating) {
     }
     if (activeparam == 'tempcelsius') {
         return rating > 30 ? redIcon :
-        //rating > 6.5 ? yellowIcon :
                      greenIcon;
     }
     if (activeparam == 'phosphate') {
@@ -89,6 +151,7 @@ function getIcon(rating) {
         rating >= 0.3 ? yellowIcon :
                     greenIcon;
     }};
+
 
 var redIcon = L.divIcon({ 
     className : 'redcircle',
@@ -111,9 +174,6 @@ function mostrecent(allsites) {
     })
 };
 
-//mostrecent([1,2,3,4,5,6,7,8,9,10,11,12])
-
-//endtesting
 
 // parameter selection
 $('.side-nav-item').on('click', onParamButtonClick);
@@ -145,9 +205,9 @@ function drawgraph() {
             lineThickness: 0,
         },
         axisY: {
-            gridColor: "darkgrey",
+            gridColor: "#D7DBDD",
             gridThickness: 1,
-            gridDashType: "dash",
+            gridDashType: "solid",
             lineThickness: 0,
             //title: paramname[activeparam],
             //titlefontFamily: "tahoma",
@@ -174,7 +234,6 @@ function drawgraph() {
                 e.chart.data[i].remove();
                 let sitetoremove = Object.keys(names).find(key => names[key] === (e.dataSeries.name))
                 siteids.splice(siteids.indexOf(Number(sitetoremove)),1);
-                // now just figure out how to remove that one
         }}
         e.chart.render();
     }
@@ -183,26 +242,19 @@ function drawgraph() {
     $("#chartContainer").CanvasJSChart(options);
 };
 
-var param = {
-    ph: {good: 5, bad: 6}, 
-    do: {good: 8, bad: 8}, 
-    phosphate: {good: 8, bad: 9},
-    conductivity: {good: 10, bad: 9},
-    tempcelsius: {good: 12, bad: 12}}
-
 var paramname = {
     ph: {name: "pH", unit: "", description: 
     "pH indicates the alkalinity or acidity \
     of a substance from 1.0 to 14.0. Acidity increases as the pH gets lower. Changes in acidity can be caused by atmospheric deposition (acid rain), surrounding rock, and \
     certain wastewater discharges. pH affects many chemical and biological processes in the water. The largest variety of aquatic animals prefer a range of 6.5-8.0. pH outside this range stresses \
     athe physiological systems of most organisms and can reduce reproduction.",
-    range: "6.5-8.0, <9"},
+    range: "6.5-8.0"},
     do: {name: "Dissolved Oxygen", unit: "mg/L", 
-    range: ">= 6 mg/L, >3.5 mg/L", description:'Dissolved oxygen (DO) is the amount of oxygen that is present in water. Water bodies receive oxygen from the atmosphere and from aquatic plants. Respiration by aquatic animals, decomposition, and various chemical reactions consume oxygen. Sources of oxygen-consuming waste include wastewater from sewage treatment plants, stormwater runoff from farmland or urban streets, feedlots, and failing septic systems.  DO levels fluctuate seasonally and over a 24-hour period. They vary with water temperature and altitude. Cold water holds more oxygen than warm water.  While each organism has its own DO tolerance range, generally, DO levels below 3 milligrams per liter (mg/L) are of concern and waters with levels below 1 mg/L are considered hypoxic and usually devoid of life.'},
+    range: ">= 6 mg/L", description:'Dissolved oxygen (DO) is the amount of oxygen that is present in water. Water bodies receive oxygen from the atmosphere and from aquatic plants. Respiration by aquatic animals, decomposition, and various chemical reactions consume oxygen. Sources of oxygen-consuming waste include wastewater from sewage treatment plants, stormwater runoff from farmland or urban streets, feedlots, and failing septic systems.  DO levels fluctuate seasonally and over a 24-hour period. They vary with water temperature and altitude. Cold water holds more oxygen than warm water.  While each organism has its own DO tolerance range, generally, DO levels below 3 milligrams per liter (mg/L) are of concern and waters with levels below 1 mg/L are considered hypoxic and usually devoid of life.'},
     phosphate: {name: "Phosphate", unit: "mg/L", 
-    range: "< 0.3 mg/L, < 0.6 mg/L", description:'Both phosphorus and nitrogen are essential nutrients for the plants and animals that make up the aquatic food web.  A modest increase in phosphorus can, under the right conditions, set off a whole chain of undesirable events in a stream including accelerated plant growth, algae blooms, low dissolved oxygen, and the death of certain fish, invertebrates, and other aquatic animals. There are many sources of phosphorus, both natural and human. These include soil and rocks, wastewater treatment plants, runoff from fertilized lawns and cropland, failing septic systems, runoff from animal manure storage areas, disturbed land areas, drained wetlands, water treatment, and commercial cleaning preparations. In nature, phosphorus usually exists as part of a phosphate molecule (PO4).'},
+    range: "< 0.3 mg/L", description:'Both phosphorus and nitrogen are essential nutrients for the plants and animals that make up the aquatic food web.  A modest increase in phosphorus can, under the right conditions, set off a whole chain of undesirable events in a stream including accelerated plant growth, algae blooms, low dissolved oxygen, and the death of certain fish, invertebrates, and other aquatic animals. There are many sources of phosphorus, both natural and human. These include soil and rocks, wastewater treatment plants, runoff from fertilized lawns and cropland, failing septic systems, runoff from animal manure storage areas, disturbed land areas, drained wetlands, water treatment, and commercial cleaning preparations. In nature, phosphorus usually exists as part of a phosphate molecule (PO4).'},
     conductivity: {name: "Conductivity", unit: "µs/cm", 
-    range: "150-500 µs/cm, <1900 µs/cm", description:'Conductivity is a measure of the ability of water to pass an electrical current. Because dissolved salts and other inorganic chemicals conduct electrical current, conductivity increases as salinity increases. Conductivity is also affected by temperature: the warmer the water, the higher the conductivity.  Generally, human disturbance tends to increase the amount of dissolved solids entering waters which results in increased conductivity. Conductivity is measured in microsiemens per centimeter (µs/cm). The conductivity of rivers in the United States generally ranges from 50 to 1500 µs/cm. Streams supporting good mixed fisheries have a range between 150 and 500 µs/cm. Conductivity outside this range could indicate that the water is not suitable for certain species of fish or macroinvertebrates.'},
+    range: "150-500 µs/cm", description:'Conductivity is a measure of the ability of water to pass an electrical current. Because dissolved salts and other inorganic chemicals conduct electrical current, conductivity increases as salinity increases. Conductivity is also affected by temperature: the warmer the water, the higher the conductivity.  Generally, human disturbance tends to increase the amount of dissolved solids entering waters which results in increased conductivity. Conductivity is measured in microsiemens per centimeter (µs/cm). The conductivity of rivers in the United States generally ranges from 50 to 1500 µs/cm. Streams supporting good mixed fisheries have a range between 150 and 500 µs/cm. Conductivity outside this range could indicate that the water is not suitable for certain species of fish or macroinvertebrates.'},
     tempcelsius: {name:"Temperature", unit:"°C", 
     range: "< 30°C", description:'The rates of biological and chemical processes depend on temperature. Aquatic organisms from microbes to fish are dependent on certain temperature ranges for their optimal health. Optimal temperatures for fish depend on the species. Many organisms are unable to survive in temperatures above about 30°C. Causes of temperature change include weather, removal of shading streambank vegetation, impoundments, discharge of cooling water, urban stormwater, and groundwater inflows to the stream. Over time, an area’s climate has the strongest natural influence on a stream’s temperature.'},
 }
@@ -280,6 +332,10 @@ function updateData(toUpdate) {
     function addSeries(readings, name) {
         const series = {
             type: "spline",
+            lineDashType: "solid",
+            lineThickness: 0.5,
+            connectNullData: false,
+            markerSize: 9,
             showInLegend: true,
             dataPoints: readings,
             name: name
