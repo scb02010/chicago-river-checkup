632f0efdcc4cea811141cce0d964b17ecce69410
sarahcmap <sbuchhorn@cmap.illinois.gov>
2022-08-28 13:52:06 -0500

    -data update -change scale (todo: fix 2017 temps)

--- app/dist/watermap.js
+++ app/dist/watermap.js
@@ -2,7 +2,7 @@ var mymap = L.map('watermap').setView([41.94, -87.64], 11)
 
 var mytoken = 'pk.eyJ1Ijoic2NiMDIwMTAiLCJhIjoiY2pzM2Y2eHdjMmVuaTQ1bzN6OGE3MnJrYiJ9.5QDjNpLmtS-Y9N3nP2rLdQ'
 
-var lastupdate = 'June 5, 2022'
+var lastupdate = 'August 28, 2022'
 $('#lastupdate').text(lastupdate);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',{
@@ -228,8 +228,8 @@ function drawgraph() {
         axisX: {
             valueFormatString: "MMM YYYY",
             lineThickness: 0,
-            minimum: new Date(2017, 2, 1), 
-            maximum: new Date(2022, 6, 1)
+            minimum: new Date(2018, 2, 1), 
+            maximum: new Date(2022, 9, 1)
         },
         axisY: {
             gridColor: "#D7DBDD",
