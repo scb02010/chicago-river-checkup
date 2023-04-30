2eafdb9f563c76a5eb749f79a9a833dcf4cb96a6
sarahcmap <sbuchhorn@cmap.illinois.gov>
2023-04-30 10:21:54 -0500

    data update

--- app/dist/watermap.js
+++ app/dist/watermap.js
@@ -2,7 +2,7 @@ var mymap = L.map('watermap').setView([41.94, -87.64], 11)
 
 var mytoken = 'pk.eyJ1Ijoic2NiMDIwMTAiLCJhIjoiY2pzM2Y2eHdjMmVuaTQ1bzN6OGE3MnJrYiJ9.5QDjNpLmtS-Y9N3nP2rLdQ'
 
-var lastupdate = 'March 26, 2023'
+var lastupdate = 'April 30, 2023'
 $('#lastupdate').text(lastupdate);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',{
@@ -229,7 +229,7 @@ function drawgraph() {
             valueFormatString: "MMM YYYY",
             lineThickness: 0,
             minimum: new Date(2018, 2, 1), 
-            maximum: new Date(2023, 5, 1)
+            maximum: new Date(2023, 6, 1)
         },
         axisY: {
             gridColor: "#D7DBDD",
